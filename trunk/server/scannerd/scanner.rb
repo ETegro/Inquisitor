c819c4b3c6ad0d4accbfa1166f974ab44119ec43
Sergey Matveev <stargrave@stargrave.org>
2012-02-06 16:46:57 +0400

    trivia

--- trunk/server/scannerd/scanner.rb
+++ trunk/server/scannerd/scanner.rb
@@ -1,19 +1,4 @@
 #!/usr/bin/env ruby
-# server/scannerd/scanner.rb - A part of Inquisitor project
-# Copyright (C) 2004-2009 by Iquisitor team 
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'net/http'
 require 'rexml/document'
@@ -46,6 +31,7 @@ class Scanner
 		$PAIRED_SCANS.each { |ps|    
 			if @svals.keys.sort! == ps.sort! then
 				preprocess_vals()
+#				@svals.keys.each { |k| @svals[k].sub!(/^[S|A|O|T|I|P]/, '') }
 				case ps
 				    when ['C', 'S'] then
 					open("http://#{$SERVER_ADDR}/computers/add_component/#{@svals['S']}.xml?type=Power+Supply&vendor=ColdWatt&model=CWA2-0650-10-IV01&serial=#{@svals['C']}")
@@ -76,9 +62,7 @@ class Scanner
 					
 					open("http://#{$SERVER_ADDR}/computers/set_assembler/#{@svals['S']}.xml?assembler_id=#{@svals['A']}")
 					open("http://#{$SERVER_ADDR}/computers/set_tester/#{@svals['S']}.xml?tester_id=#{@svals['T']}")
-										
 					addrs.each{ |a| Thread.new(a) { |la| send2ip_addr(la, @svals['S']) } }
-					    
 				end
 
 				return 0
