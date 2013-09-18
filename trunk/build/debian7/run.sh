ed7f2569c796fdde9fe2a1e924e198ddbe537142
Difrex(Denis Zheleztsov) <dz@etegro.com>
2013-09-18 17:30:52 +0400

    Added debian7 target

---
+++ trunk/build/debian7/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+DIR=`pwd`
+LIVEDIR="${DIR}/live"
+BUILDDIR="${DIR}/build"
+
+. ${DIR}/config
+
+function clean() {
+	cd $LIVEDIR
+	lb clean
+}
+
+function build() {
+	mkdir -p $LIVEDIR
+	cd $LIVEDIR
+	lb config \
+		--bootstrap debootstrap \
+		--system live
+	sed s/PLATFORM/$ARCH/g < ../config.bootstrap > ../bootstrap.1
+	sed s/DEB_DISTR/$DEB_VERSION/g < ../bootstrap.1 > config/bootstrap
+	rm -f ../bootstrap.1
+	mkdir -p $BUILDDIR
+	cd $BUILDDIR
+	cp -rv ../packages $BUILDDIR/
+	for i in `ls ${BUILDDIR}/packages/`; do
+		cd ${BUILDDIR}/packages/${i}
+		dpkg-buildpackage
+		
+}
